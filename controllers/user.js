aa022cdb7a60d46d3ffd38ba4cd5293eb1c406c0
Toniin <antonin.chaudiere@hotmail.fr>
2023-05-23 16:41:05 +0200

    Add bcrypt + hash password when created

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,29 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
 exports.signup = (request, response) => {
-  const userObject = request.body;
-  const user = new User(userObject);
-  user
-    .save()
-    .then(() =>
-      response
-        .status(201)
-        .json({ user: user })
-        .send(console.log("User created successfully !"))
-    )
-    .catch((error) => response.status(400).json({ error }));
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: request.body.email,
+        password: hash,
+      });
+
+      user
+        .save()
+        .then(() =>
+          response
+            .status(201)
+            .json({ user: user })
+            .send(console.log("User created successfully !"))
+        )
+        .catch((error) => response.status(400).json({ error }));
+    })
+    .catch((error) => response.status(500).json({ error }));
 };
 
 exports.login = (request, response) => {
@@ -25,12 +34,27 @@ exports.login = (request, response) => {
           .status(401)
           .json({ message: "Identifiant ou mot de passe incorrecte." });
       } else {
-        response.status(200).json({
-          userId: user._id,
-          token: jwt.sign({ userId: user._id }, process.env.JWT_PRIVATE_KEY, {
-            expiresIn: "1h",
-          }),
-        });
+        bcrypt
+          .compare(request.body.password, user.password)
+          .then((valid) => {
+            if (valid) {
+              response.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                  { userId: user._id },
+                  process.env.JWT_PRIVATE_KEY,
+                  {
+                    expiresIn: "1h",
+                  }
+                ),
+              });
+            } else {
+              response
+                .status(401)
+                .json({ message: "Identifiant ou mot de passe incorrecte." });
+            }
+          })
+          .catch((error) => response.status(500).json({ error }));
       }
     })
     .catch((error) => response.status(500).json({ error }));
